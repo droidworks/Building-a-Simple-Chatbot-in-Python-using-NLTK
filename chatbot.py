c2fe755bd16e7149ee3fe9e5f398db682845aabb
linges <eco.varan@gmail.com>
2019-05-31 14:13:50 +0800

    Update chatbot.py
    Updated bot creds

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# # Meet Robo: your friend
+# # Meet Dev: your friend
 
 import nltk
 import io
@@ -80,25 +80,25 @@ def response(user_response):
 
 
 flag=True
-print("ROBO: My name is Robo. I will answer your queries about Chatbots. If you want to exit, type Bye!")
+print("Dev: My name is Dev. I will do my best to answer your queries about Chatbots. If you want to exit, type Bye!")
 
 while(flag==True):
     user_response = input()
     user_response=user_response.lower()
     if(user_response!='bye'):
-        if(user_response=='thanks' or user_response=='thank you' ):
+        if(user_response=='thanks' or user_response=='thank you' or user_response=='merci' or user_response=='danke' ):
             flag=False
-            print("ROBO: You are welcome..")
+            print("Dev: You are welcome..")
         else:
             if(greeting(user_response)!=None):
-                print("ROBO: "+greeting(user_response))
+                print("Dev: "+greeting(user_response))
             else:
-                print("ROBO: ",end ="")
+                print("Dev: ",end ="")
                 print(response(user_response))
                 sent_tokens.remove(user_response)
     else:
         flag=False
-        print("ROBO: Bye! take care..")    
+        print("Dev: Au revoir! Se a demain!")    
         
         
 
